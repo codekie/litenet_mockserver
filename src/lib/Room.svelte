b566bad7ec6572c95b0daa385e816e6925de6995
Code Kie <codekie@users.noreply.github.com>
2022-08-31 18:30:00 +0200

    feat: show coords of luminaires on GUI

--- src/lib/Room.svelte
+++ src/lib/Room.svelte
@@ -3,11 +3,12 @@
 </script>
 
 <style lang="scss">
-  .lm-room { display: flex; flex-flow: row nowrap; padding: 2rem; border: 1px solid #888;
+  .lm-room { display: flex; flex-flow: row nowrap; padding: 2rem; border: 1px solid #888; border-radius: 4px;
     .lm-col { display: flex; flex-flow: column nowrap; padding: 0 20px;
       .lm-cell { display: flex; flex-flow: row nowrap; justify-content: center; margin: 1px 0;
-        .lm-luminaire { display: inline-block; width: 1em; height: 1.8em;
-          transition: background-color ease-in-out 200ms;
+        .lm-luminaire { display: inline-block; width: 0.9rem; height: 1.8rem;
+           transition: background-color ease-in-out 200ms;
+          .lm-coords { position: relative; display: inline-block; left: 1.3rem; font-size: 0.8em; color: rgba(255,255,255,0.4); }
         }
       }
     }
@@ -19,15 +20,17 @@
   <div>loading</div>
 {:then}
   <div class="lm-room">
-  {#each $room as cols}
+  {#each $room as cols, idxCol}
     <div class="lm-col">
-      {#each cols as luminaire}
+      {#each cols as luminaire, idxRow}
         <div class="lm-cell">
           <div class="lm-luminaire"
                data-name="{luminaire.name}"
                style="background-color: rgba(255, 255, 255, {
                   ($luminaires.get(luminaire.name).level / 100) * 0.8 + 0.2
-               })"></div>
+               })">
+            <span class="lm-coords">{idxCol},{idxRow}</span>
+          </div>
         </div>
       {/each}
     </div>
